f7ee730091d98fcffc1f60f4bb5092315aa8b258
Leonardo Tabor <leotabor13@gmail.com>
2025-01-10 13:52:41 +0800

    add session

--- server/server.js
+++ server/server.js
@@ -6,15 +6,25 @@ import { authSetup, handleAuth } from './src/middleware/auth.js';
 import { handleError } from './src/middleware/error.js';
 import routes from './src/routes/index.js';
 import { init } from '@kinde/management-api-js';
-
 const app = express();
 const PORT = 8080;
-
+import session from 'express-session';
 // Initialize Kinde Auth Client Session
 authSetup(app);
 
 // Initialize Kinde Auth Management API
 init();
+
+const MemoryStore = require('memorystore')(session)
+
+app.use(session({
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  }),
+  resave: false,
+  secret: 'keyboard cat'
+}))
 
 app.use(cors());
 
