1240754f0304d8b2ff9cc4dc056c0851af69c00f
Leonardo Tabor <81302188+leotabor18@users.noreply.github.com.>
2025-02-01 02:36:25 +0800

    my final solution

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,7 +2,10 @@ import MainHeader from './components/client/header';
 
 const Home = async () => {
   console.log('Hello');
-  
+  const solution1 = () => {
+
+  }
+
   return (
     <div className="flex flex-col w-full">
       <MainHeader />
